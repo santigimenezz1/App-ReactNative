79c3849d67a78f1f771e27c90ff35fe9d4e3b6a5
santiago <santiago.manuel.gimenez97@gmail.com>
2024-07-11 18:28:54 +0200

    ESTILOS DEL HOME TERMINADOS

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import TarjetaNivel from "../../components/TarjetaNivel/TarjetaNivel.jsx";
 import styles from "../../pages/Home/Home.js";
 import { RFValue } from "react-native-responsive-fontsize";
 import TarjetaIngresoCodigo from './TarjetaIngesoCodigo/TarjetaingresoCodigo.jsx';
+import TarjetaConsejos from '../../components/TarjetaConsejos/TarjetaConsejos.jsx';
 
 
 const Home = ( {navigation} ) => {
@@ -16,7 +17,7 @@ const Home = ( {navigation} ) => {
       <ScrollView style={styles.main} contentContainerStyle={styles.contentContainer}>
       <TarjetaIngresoCodigo />
       <Text style={{fontSize: RFValue(18), color:"white",marginTop:30, marginBottom:40, textAlign:"center",fontFamily: 'NunitoSans_400Regular', letterSpacing:0.7 }} >Encontraras el código unico en el folleto que viene con el producto</Text>
-        <Text style={{fontSize: RFValue(20), color:"white",     fontFamily: 'NunitoSans_400Regular', }}>Imprescindibles</Text>
+        <Text style={{fontSize: RFValue(20), color:"white", fontFamily: 'NunitoSans_400Regular', }}>Imprescindibles</Text>
         <TarjetaCalentamiento navigation={navigation}/>
         <Text style={{fontSize: RFValue(20), color:"white",marginTop:30, fontFamily: 'NunitoSans_400Regular' }}>Ejercicios</Text>
         <TarjetaNivel nivel={"Nivel 1"} tiempo={"30:25"} navigation={navigation} />
@@ -24,6 +25,9 @@ const Home = ( {navigation} ) => {
         <TarjetaNivel nivel={"Nivel 3"} tiempo={"52:06"} navigation={navigation}/>
         <TarjetaNivel nivel={"Nivel Avanzado"} tiempo={"52:06"} navigation={navigation}/>
         <TarjetaNivel nivel={"Retos"} tiempo={"52:06"} navigation={navigation}/>
+        <View style={{marginTop:30}}>
+        <TarjetaConsejos />
+        </View>
       </ScrollView>
     </View>
   );
