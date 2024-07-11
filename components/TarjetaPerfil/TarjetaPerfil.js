90c9e97bf251d86daf7be5f2984986e5358a4e5f
santiago <santiago.manuel.gimenez97@gmail.com>
2024-07-11 12:04:11 +0200

    tarjeta perfil v1

--- components/TarjetaPerfil/TarjetaPerfil.js
+++ components/TarjetaPerfil/TarjetaPerfil.js
@@ -2,8 +2,6 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container__tarjetaPerfil:{
-        borderWidth:1,
-        borderColor:"red",
         width:"100%",
         height:600, 
         position:"relative", 
@@ -15,8 +13,6 @@ const styles = StyleSheet.create({
         width:"100%",
         height:400,
         position:"absolute",
-        borderWidth:6,
-        borderColor:"green",
         zIndex:2,
     }
 })
