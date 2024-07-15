4e0de71917da741ca53e11aa3ba943cf5a8106a8
santiago <santiago.manuel.gimenez97@gmail.com>
2024-07-16 01:03:30 +0200

    funcion enviarUsuario agregada

--- pages/LoginUsuarios/InicioSesion/InicioSesion.jsx
+++ pages/LoginUsuarios/InicioSesion/InicioSesion.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, Pressable, Image } from 'react-native';
 import styles from './InicioSesion';
 
-const InicioSesion = ( {setEstado} ) => {
+const InicioSesion = () => {
+  const [valueEmail, setValueEmail] = useState("")
+  const [valuePassword, setValuePassword] = useState("")
+
+
+   const EnviarUsuario = () => {
+    const user = {
+      email: valueEmail,
+      password: valuePassword
+    }
+    console.log(user)
+   }
+
+
+
   return (
     <View style={styles.container__inicioSesion}>
       <View style={styles.container__form}>
-        <TextInput style={styles.input} placeholderTextColor={"white"} placeholder='Email'></TextInput>
-        <TextInput style={styles.input} placeholderTextColor={"white"} placeholder='Contraseña'></TextInput>
+        <TextInput  onChangeText={(text)=>setValueEmail(text)} style={styles.input} placeholderTextColor={"white"} placeholder='Email'></TextInput>
+        <TextInput  onChangeText={(text)=>setValuePassword(text)} style={styles.input} placeholderTextColor={"white"} placeholder='Contraseña'></TextInput>
         <View style={styles.container__form}>
-        <Pressable style={styles.botonLoginUsuario}>
+        <Pressable onPress={()=>EnviarUsuario()}  style={styles.botonLoginUsuario}>
             <Text style={styles.botonText}>
                 Iniciar sesión
             </Text>
         </Pressable>
-        <Pressable  style={styles.botonLoginGoogle}>
+        <Pressable   style={styles.botonLoginGoogle}>
             <Image width={30} height={30} source={{uri:"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1720772208/APP%20ALFOMBRA%20DE%20FUTBOL%20AMAZON/cromo_doubjp.png"}}></Image>
             <Text style={styles.botonText}>
                 Continunar usando google
