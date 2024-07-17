4a234b3a9dd82c6ce42f8444826ba4c1cbebab4c
santiago <santiago.manuel.gimenez97@gmail.com>
2024-07-17 02:50:46 +0200

    LOGICA CODIGO DESBLOQUEO V2

--- pages/Home/DetalleNivel/TarjetaNivelDetalle/TarjetaNivelDetalle.jsx
+++ pages/Home/DetalleNivel/TarjetaNivelDetalle/TarjetaNivelDetalle.jsx
@@ -21,13 +21,15 @@ const TarjetaNivelDetalle = ( {nivel, tiempo, navigation, ejercicio} ) => {
             <Text style={styles.text}>{nivel}</Text>
             <Text style={styles.texth2}>{tiempo}</Text>
             </View>
-     
+            {
+                !closed &&
             <View style={styles.container__bloqueado}> 
             <View style={{width:"100%",display:"flex",alignItems:"center"}}>
             <Image  width={22} height={22} source={{uri:"https://res.cloudinary.com/dcf9eqqgt/image/upload/v1720478069/APP%20ALFOMBRA%20DE%20FUTBOL%20AMAZON/cerrar_qrawqr.png"}}></Image>
             </View>
             <Text style={{color:"white",fontFamily: 'NunitoSans_400Regular', letterSpacing:1}}>Bloqueado</Text>
             </View>
+            }
         </Pressable>
         </View>
     )
